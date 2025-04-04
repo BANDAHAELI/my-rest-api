dbb577f762d9d08af9ddab0d93d80584318a94ba
BANDAHAELI <ali.khan.urf.bandali@gmail.com>
2025-04-04 07:38:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'SARKAR'// yourname
+global.MONGO_DB_URI = "mongodb+srv://wasitech10:WASImd##12@cluster0.eclhxiz.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "alikhanurfbandali@gmail.com" //email
+global.email_password = "@iamdadl123" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
