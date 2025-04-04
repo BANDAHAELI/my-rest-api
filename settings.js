9d3bee392590d1702e7c1171012e11406a423b4d
BANDAHAELI <ali.khan.urf.bandali@gmail.com>
2025-04-04 10:02:22 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'SARKAR'// yourname
+global.apikey = ['sarkar', 'bandaheali'];
 global.MONGO_DB_URI = "mongodb+srv://shahzadabaloooch:DfdSqWiEsbDxefG9@cluster0.jae9sf9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "alikhanurfbandali@gmail.com" //email
